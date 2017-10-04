f153525aa819f63a6d4991e973451146ee24d826
dsp <dsp@2f30.org>
2017-10-04 15:23:05 -0700

    counting advertizements and withdraws

--- cmd/dumpanalyzer/dumpanalyzer.go
+++ cmd/dumpanalyzer/dumpanalyzer.go
@@ -33,14 +33,16 @@ func init() {
 }
 
 type PrefixDateValues struct {
-	date       time.Time          //bucket
-	prefCounts map[string]int64   //this will be populated in the end
-	peerAdv    map[string][]int32 //advertizement events where int32 is the peer ASN
+	date          time.Time          //bucket
+	prefAdvCounts map[string]int64   //this will be populated in the end
+	prefWdrCounts map[string]int64   //this will be populated in the end
+	peerAdv       map[string][]int32 //advertizement events where int32 is the peer ASN
 }
 
 func NewPrefixDateValues(date time.Time) PrefixDateValues {
 	return PrefixDateValues{
 		date,
+		make(map[string]int64),
 		make(map[string]int64),
 		make(map[string][]int32),
 	}
@@ -61,7 +63,23 @@ func NewPrefixHistoryMgr(a time.Duration) *PrefixHistoryMgr {
 func (p *PrefixHistoryMgr) Add(a gobgpdump.PrefixHistory) {
 	for _, ev := range a.Events {
 		bucket_ind := p.GetBucket(ev.Timestamp)
-		fmt.Printf("event with ts:%s will go to bucket with ts:%s from %d buckets\n", ev.Timestamp, p.buckets[bucket_ind].date, len(p.buckets))
+		//fmt.Printf("event with ts:%s will go to bucket with ts:%s from %d buckets\n", ev.Timestamp, p.buckets[bucket_ind].date, len(p.buckets))
+		//increase prefix count
+		buck := p.buckets[bucket_ind]
+		if ev.Advertized {
+			if count, ok := buck.prefAdvCounts[a.Pref]; ok {
+				buck.prefAdvCounts[a.Pref] = count + 1
+			} else {
+				buck.prefAdvCounts[a.Pref] = 1
+			}
+		} else {
+			if count, ok := buck.prefWdrCounts[a.Pref]; ok {
+				buck.prefWdrCounts[a.Pref] = count + 1
+			} else {
+				buck.prefWdrCounts[a.Pref] = 1
+			}
+		}
+
 	}
 }
 
